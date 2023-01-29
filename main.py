f3cf459054f9817827d2a55d1da598ba43a71d44
norindes <norindes@gmail.com>
2023-01-28 19:35:06 -0600

    updated code

--- main.py
+++ main.py
@@ -1,86 +1,90 @@
-from telegram.ext import *
-import keys
-import pandas_datareader as web
+import logging
+from telegram import __version__ as TG_VER
+from jinjernot import *
 
-#Init
-print ("Starting up SabarobeBot...")
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
 
-#Funciones
-def start_command(update, context):
-    update.message.reply_text("Welcome to SabarobeBot")
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
 
-def stock_command(update, context):
-    ticker = context.args[0]
-    data = web.DataReader(ticker, 'yahoo')
-    price = data.iloc[-1]['Close']
-    update.message.reply_text(f"The current price of {ticker} is {price:.2f}$!")
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, ConversationHandler, filters
 
-def contact_command(update, context):
-    update.message.reply_text("http://www.sabarobestudios.io")
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+TW = range(1)
 
-def help_command(update, context):
-    update.message.reply_text("""
-    Help Menu
-    /start -> Start the Bot
-    /stock -> Stock price: /stock + ticker
-    /contact -> Information about contact
-    /help -> Help menu
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Greetings {user.mention_html()} Welcome to Brigadier",
+        reply_markup=ForceReply(selective=True),
+    )
 
-    """)
+async def tweet(update: Update, context:ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text("enter tweet");
+    user = update.message.from_user
+    print(user)
+    return TW
 
 
-def handle_response(text: str) -> str:
-    if "hello" in text:
-        return "hey there"
+# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Echo the user message."""
+#     await update.message.reply_text(update.message.text)
 
-    if "Sabarobe" in text:
-        return "arre"
+async def contact_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("http://www.sabarobestudios.io")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    await update.message.reply_text("Bye")
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("token").build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("tweet", tweet))
+    application.add_handler(CommandHandler("contact", contact_command))
+    application.add_handler(CommandHandler("help", help_command))
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            TW: [MessageHandler(filters.TEXT & ~filters.COMMAND, tweet)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conv_handler)
+
     
-    if "help" in text:
-        return "Helping"
+    # on non command i.e message - echo the message on Telegram
+    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
 
 
-    return "invalid message"
-
-def handle_message(update, context):
-    message_type = update.message.chat.type
-    text = str(update.message.text).lower()
-    response = ""
-
-    print(f'User ({update.message.chat.id}) says: "{text}" in {message_type}')
-
-    
-    if message_type == "group":
-        if "@sabarobebot" in text:
-            new_text = text.replace("@sabarobebot", "").strip()
-            response = handle_response(new_text)
-    else:
-        response = handle_response(text)
-
-    update.message.reply_text(response)
-
-def error(update, context):
-    print(f'Update ({update}) caused error: {context.error}')
-
-
-if __name__ == '__main__':
-    updater = Updater(keys.token, use_context=True)
-    dp = updater.dispatcher
-
-    #Command
-    dp.add_handler(CommandHandler('start', start_command))
-    dp.add_handler(CommandHandler('stock', stock_command))
-    dp.add_handler(CommandHandler('contact', contact_command))
-    dp.add_handler(CommandHandler('help', help_command))
-    
-
-    #Message
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
-
-    #error
-    dp.add_error_handler(error)
-
-    #Run Bot
-    updater.start_polling() 
-    updater.idle()   
-
+if __name__ == "__main__":
+    main()
